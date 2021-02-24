a8965fbd324addb464677d8184b8ec1c95a5bac0
rasanuma <asanumadragon2000@outlook.com>
2021-02-24 18:02:51 +0900

    myroomba1

---
+++ include/myroomba1.h
@@ -0,0 +1,32 @@
+#ifndef MYTURTLE_H
+#define MYTURTLE_H
+
+#include<ros/ros.h>
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
+#include "roomba_500driver_meiji/RoombaCtrl.h"
+class Room
+{
+public:
+   Room();
+   void process();
+
+private:
+   void roomba();
+   void pose_callback(const nav_msgs::Odometry::ConstPtr &msg);
+
+   int hz;
+   int check;
+   float longsize_;
+   float dt;
+   float theta=0;
+   float kyori;
+   float time=0.0;
+
+   ros::NodeHandle nh;
+   ros::NodeHandle private_nh;
+   ros::Subscriber sub_pose;
+   ros::Publisher pub_twist;
+   nav_msgs::Odometry now_pose;
+};
+#endif
---
+++ src/myroomba1.cpp
@@ -0,0 +1,67 @@
+#include<myroomba1.h>
+
+Room::Room():private_nh("~")
+{
+    private_nh.param("hz",hz,{10});
+
+    dt = 1/float(hz);
+
+    sub_pose = nh.subscribe("roomba/odometry",10,&Room::pose_callback,this);
+
+    pub_twist = nh.advertise<geometry_msgs::Twist>("/roomba/comtrol",1);
+
+    longsize_ = 0;
+
+    kyori = 10;
+}
+
+void Room::pose_callback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    now_pose = *msg;
+}
+
+void Room::roomba()
+{
+    longsize_ = now_pose.pose.pose.position.x;
+    roomba_500driver_meiji::RoombaCtrl v;
+    time += dt;
+    //theta += v.angular.z;
+    v.cntl.linear.x = 0.1;
+    if(longsize_>=kyori && !(theta >= 2*M_PI)) {
+        v.cntl.linear.x = 0;
+        v.cntl.angular.z =M_PI;
+        /*
+        if(theta <= 3.8*M_PI/N){
+            v.angular.z = 1;
+        }
+        */
+        Room::theta += dt*v.cntl.angular.z;
+        if(theta >= 2*M_PI){
+            v.cntl.linear.x = 0.1;
+            //time=0;
+            //theta=0;
+        }
+    }
+    //printf("%f\n", theta);
+    pub_twist.publish(v);
+}
+
+void Room::process()
+{
+    ros::Rate loop_rate(hz);
+    while (ros::ok())
+    {
+        // go_straight();
+        roomba();
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "roomba");
+    Room room;
+    room.process();
+    return 0;
+}
